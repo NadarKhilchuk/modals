e9708397383c495f6118ad438f7e47f2a11a8203
Nadar <nadarhilcuk@gmail.com>
2023-12-25 19:07:19 +0100

    add model form

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,24 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
+function modalClick() {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]')
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+
+  window.onclick = function (event) {
+    if (event.target == refs.modal) {
+      refs.modal.classList.add('is-hidden');
+      document.body.classList.add('no-scroll');
     }
-  })();
+  };
+}
+
+modalClick();
